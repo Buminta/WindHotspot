dffc12fb965eafea868eb311de59f12e354e5244
Tan Bui <tanbm.hut@gmail.com>
2016-07-01 15:53:16 +0700

    Create WindHotspot.java

---
+++ src/main/java/xyz/windjs/android/utils/WindHotspot.java
@@ -0,0 +1,55 @@
+package xyz.windjs.android.utils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import xyz.windjs.android.ui.Call;
+
+/**
+ * Created by me866chuan on 7/1/16.
+ */
+public class WindHotspot {
+
+    HashMap<String,ArrayList<Call>> listHashMap = new HashMap<>();
+
+    public static WindHotspot windHotspot;
+
+    public static WindHotspot getInstance(){
+        if(windHotspot == null){
+            windHotspot = new WindHotspot();
+        }
+        return windHotspot;
+    }
+
+    public void bind(String name, Call call){
+        for(int i=0; i<listHashMap.size(); i++){
+            if (listHashMap.containsKey(name)){
+                listHashMap.get(name).add(call);
+                return;
+            }
+        }
+        listHashMap.put(name, new ArrayList<Call>());
+        listHashMap.get(name).add(call);
+    }
+
+    public void unbind(String name, Call call){
+        for(int i=0; i<listHashMap.size(); i++){
+            if (listHashMap.containsKey(name)){
+                listHashMap.get(name).remove(call);
+                return;
+            }
+        }
+    }
+
+    public void call(String name, Object... objectes){
+        for(int i=0; i<listHashMap.size(); i++){
+            if (listHashMap.containsKey(name)){
+                ArrayList<Call> list = listHashMap.get(name);
+                for(int j=0; j<list.size(); j++){
+                    list.get(j).run(objectes);
+                }
+                return;
+            }
+        }
+    }
+}
